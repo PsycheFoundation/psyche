552bbb31cd88003472365461621728588f0766f6
Dylan Socolobsky <dylan.socolobsky@lambdaclass.com>
2025-05-07 14:59:37 -0300

    centralized-server: throw error if no data providers present

--- architectures/centralized/server/src/app.rs
+++ architectures/centralized/server/src/app.rs
@@ -185,7 +185,7 @@ impl App {
                     ..
                 }) => {
                     // If you fail with the 1st data provider at the start don't even bother trying with the fallbacks
-                    let data_location = &data_locations[0];
+                    let data_location = data_locations.get(0).ok_or_else(|| anyhow!("No data location provided"))?;
                     if let LLMTrainingDataType::Finetuning = data_type {
                         panic!("Finetuning is not supported yet.")
                     }
