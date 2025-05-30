236c158743eb455cbb6e99d5e762c811da7e1a73
Dylan Socolobsky <dylan.socolobsky@lambdaclass.com>
2025-05-30 18:16:39 -0300

    Send time-per-batch to coordinator & run basic consensus algo plus assign scores.
    Still missing really assigning batch sizes.

--- shared/coordinator/src/lib.rs
+++ shared/coordinator/src/lib.rs
@@ -15,6 +15,7 @@ pub use coordinator::{
     CoordinatorProgress, HealthChecks, Round, RunState, TickResult, Witness, WitnessBloom,
     WitnessEvalResult, WitnessMetadata, BLOOM_FALSE_RATE, NUM_STORED_ROUNDS,
     SOLANA_MAX_NUM_CLIENTS, SOLANA_MAX_NUM_WITNESSES, SOLANA_MAX_STRING_LEN,
+    TRAINING_TIMES_SLICE_SIZE,
 };
 pub use data_selection::{
     assign_data_for_state, get_batch_ids_for_node, get_batch_ids_for_round, get_data_index_for_step,
