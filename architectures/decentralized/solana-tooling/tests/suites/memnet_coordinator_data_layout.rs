7f9c164d2dc8502e3810231c62cd857b073452e5
Vincent Brunet <vincent.brunet.us@gmail.com>
2025-06-21 17:40:27 +0900

    fix-tests

--- architectures/decentralized/solana-tooling/tests/suites/memnet_coordinator_data_layout.rs
+++ architectures/decentralized/solana-tooling/tests/suites/memnet_coordinator_data_layout.rs
@@ -1,12 +1,10 @@
-use std::fs;
-
 use psyche_solana_coordinator::coordinator_account_from_bytes;
 
 #[tokio::test]
 pub async fn run() {
     let coordinator_dump =
-        fs::read("tests/fixtures/coordinator-account-v0.so").unwrap();
+        include_bytes!("../fixtures/coordinator-account-v0.so");
     let coordinator_account =
-        coordinator_account_from_bytes(&coordinator_dump).unwrap();
+        coordinator_account_from_bytes(coordinator_dump).unwrap();
     assert_eq!(coordinator_account.nonce, 563234);
 }
