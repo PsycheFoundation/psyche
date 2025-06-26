3db65633898bc1b76e0b0a4b1a1333898dda08bf
IAvecilla <ignacio.avecilla@lambdaclass.com>
2025-06-26 13:49:55 -0300

    Try to use garnix cache

--- scripts/push-client-image.sh
+++ scripts/push-client-image.sh
@@ -4,6 +4,8 @@ DOCKER_REPOSITORY="ignacioavecilla/psyche-solana-client"
 DOCKER_USERNAME="ignacioavecilla"
 
 echo "dckr_pat_LS668OPTwTAXR3b3pCLJit5b1O0" | docker login -u "$DOCKER_USERNAME" --password-stdin
-nix build .#docker-psyche-solana-client --out-link nix-results/docker-psyche-solana-client
-nix-results/docker-psyche-solana-client | docker load
+IMAGE_PATH=$(cat image-path.txt)
+docker load < "$IMAGE_PATH"
+IMAGE_ID=$(docker images -q | head -n1)
+docker tag "$IMAGE_ID" ignacioavecilla/psyche-solana-client:latest
 docker push "$DOCKER_REPOSITORY"
