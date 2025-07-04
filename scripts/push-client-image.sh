83880d40b644332f89c3e6820cb0c819b7d19ce9
IAvecilla <ignacio.avecilla@lambdaclass.com>
2025-07-04 01:27:49 -0300

    Use docker test key secret in script

--- scripts/push-client-image.sh
+++ scripts/push-client-image.sh
@@ -3,7 +3,6 @@ set -eu
 DOCKER_REPOSITORY="ignacioavecilla/psyche-solana-client"
 DOCKER_USERNAME="ignacioavecilla"
 
-echo "dckr_pat_LS668OPTwTAXR3b3pCLJit5b1O0" | docker login -u "$DOCKER_USERNAME" --password-stdin
 IMAGE_PATH=$(cat image-path.txt)
 docker load < "$IMAGE_PATH"
 IMAGE_ID=$(docker images -q | head -n1)
