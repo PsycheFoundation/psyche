eb91523c08419774841a26f5da8dc69541992388
Ari Lotter <arilotter@gmail.com>
2025-07-14 17:20:59 -0400

    nix: add MacOS support to the dev environment

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -3,30 +3,38 @@
   system ? null,
   lib ? inputs.nixpkgs.lib,
 }:
-lib.optionalAttrs (system != null) { inherit system; }
-// {
-  overlays = [
-    inputs.rust-overlay.overlays.default
-    inputs.nix-gl-host.overlays.default
-    (final: prev: {
-      python312Packages = prev.python312Packages.override {
-        overrides = pyfinal: pyprev: rec {
-          torch = pyprev.torch-bin;
-        };
-      };
-    })
-    (
-      final: prev:
-      import ./pkgs.nix {
-        pkgs = prev;
-        inherit inputs;
-      }
-    )
-  ];
 
-  config = {
-    allowUnfree = true;
-    cudaSupport = true;
-    cudaVersion = "12.8";
-  };
-}
+let
+  cudaSupported = builtins.elem system [ "x86_64-linux" ];
+in
+(
+  lib.optionalAttrs (system != null) { inherit system; }
+  // {
+    overlays = lib.optionals cudaSupported [ inputs.nix-gl-host.overlays.default ] ++ [
+      inputs.rust-overlay.overlays.default
+      (final: prev: {
+        python312Packages = prev.python312Packages.override {
+          overrides = pyfinal: pyprev: rec {
+            torch = pyprev.torch-bin;
+          };
+        };
+      })
+      (
+        final: prev:
+        import ./pkgs.nix {
+          pkgs = prev;
+          inherit inputs;
+        }
+      )
+    ];
+
+    config =
+      {
+        allowUnfree = true;
+      }
+      // lib.optionalAttrs cudaSupported {
+        cudaSupport = true;
+        cudaVersion = "12.8";
+      };
+  }
+)
