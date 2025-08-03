189d244ada15fd67b1359d76f11ba6994b3729e7
Vincent Brunet <vincent.brunet.us@gmail.com>
2025-08-03 21:00:05 +0900

    initial-implementation-of-info-cli

--- architectures/decentralized/solana-client/src/main.rs
+++ architectures/decentralized/solana-client/src/main.rs
@@ -1,10 +1,9 @@
 use crate::{
-    app::{AppBuilder, AppParams, TAB_NAMES, Tabs},
+    app::{AppBuilder, AppParams, Tabs, TAB_NAMES},
     backend::SolanaBackend,
 };
 
 use anchor_client::{
-    Client, Cluster,
     anchor_lang::system_program,
     solana_sdk::{
         commitment_config::{CommitmentConfig, CommitmentLevel},
@@ -13,34 +12,36 @@ use anchor_client::{
         signature::{EncodableKey, Keypair},
         signer::Signer,
     },
+    Client, Cluster,
 };
-use anyhow::{Context, Result, bail};
+use anyhow::{bail, Context, Result};
 use bytemuck::Zeroable;
 use clap::{Args, Parser, Subcommand};
-use psyche_client::{TrainArgs, print_identity_keys, read_identity_secret_key};
+use psyche_client::{print_identity_keys, read_identity_secret_key, TrainArgs};
 use psyche_coordinator::{
-    CoordinatorConfig, CoordinatorProgress, RunState, get_data_index_for_step,
+    get_data_index_for_step,
     model::{Checkpoint, HubRepo, Model},
+    CoordinatorConfig, CoordinatorProgress, RunState,
 };
-use psyche_core::{FixedString, sha256};
+use psyche_core::{sha256, FixedString};
 use psyche_network::SecretKey;
 use psyche_solana_authorizer::state::Authorization;
 use psyche_solana_coordinator::{find_coordinator_instance, logic::JOIN_RUN_AUTHORIZATION_SCOPE};
 use psyche_tui::{
-    LogOutput, ServiceInfo,
     logging::{MetricsDestination, OpenTelemetry, RemoteLogsDestination, TraceDestination},
-    maybe_start_render_loop,
+    maybe_start_render_loop, LogOutput, ServiceInfo,
 };
 use rand::SeedableRng;
 use rand_chacha::ChaCha8Rng;
 use serde::{Deserialize, Serialize};
+use serde_json::{json, to_string_pretty};
 use std::str::FromStr;
 use std::sync::Arc;
 use std::{io::Cursor, path::PathBuf, time::Duration};
 use time::OffsetDateTime;
 use tokio::{
     runtime::Builder,
-    time::{MissedTickBehavior, interval},
+    time::{interval, MissedTickBehavior},
 };
 use tracing::info;
 
@@ -277,6 +278,17 @@ enum Commands {
 
         #[clap(long, env, default_value_t = 3)]
         hub_max_concurrent_downloads: usize,
+    },
+
+    Info {
+        #[clap(flatten)]
+        cluster: ClusterArgs,
+
+        #[clap(short, long, env)]
+        run_id: String,
+
+        #[clap(long, env)]
+        treasurer_index: Option<u64>,
     },
 
     // Prints the help, optionally as markdown. Used for docs generation.
@@ -960,7 +972,8 @@ async fn async_main() -> Result<()> {
                 }
 
                 #[allow(irrefutable_let_patterns)]
-                let Model::LLM(model) = coordinator_account_state.model else {
+                let Model::LLM(model) = coordinator_account_state.model
+                else {
                     bail!("model is not an LLM, unsure how to predownload.");
                 };
 
@@ -994,6 +1007,132 @@ async fn async_main() -> Result<()> {
                 .await?;
                 println!("Model predownloaded successfully.")
             }
+            Ok(())
+        }
+        Commands::Info {
+            cluster,
+            run_id,
+            treasurer_index,
+        } => {
+            let run_id = run_id.trim_matches('"').to_string(); // Trim quotes, if any
+            let backend = SolanaBackend::new(
+                cluster.into(),
+                vec![],
+                Keypair::new().into(),
+                CommitmentConfig::confirmed(),
+            )
+            .unwrap();
+
+            let coordinator_instance_address = find_coordinator_instance(&run_id);
+            let coordinator_instance_state = backend
+                .get_coordinator_instance(&coordinator_instance_address)
+                .await?;
+            let coordinator_instance_json = json!({
+                "address": coordinator_instance_address.to_string(),
+                "join_authority": coordinator_instance_state.join_authority.to_string(),
+                "main_authority": coordinator_instance_state.main_authority.to_string(),
+            });
+
+            let coordinator_account_address = coordinator_instance_state.coordinator_account;
+            let coordinator_account_state = backend
+                .get_coordinator_account(&coordinator_account_address)
+                .await?;
+
+            let coordinator_account_json = json!({
+                "address": coordinator_account_address.to_string(),
+                "run_id": coordinator_account_state.state.coordinator.run_id,
+                "run_state": coordinator_account_state.state.coordinator.run_state.to_string(),
+                "run_clients": coordinator_account_state.state.clients_state.clients.len(),
+                "progress": {
+                    "epoch": coordinator_account_state.state.coordinator.progress.epoch,
+                    "step": coordinator_account_state.state.coordinator.progress.step,
+                },
+                "epoch": {
+                    "alive_clients": coordinator_account_state.state.coordinator.epoch_state.clients.len(),
+                    "exited_clients": coordinator_account_state.state.coordinator.epoch_state.exited_clients.len(),
+                },
+                "epoch_rates": {
+                    "current": {
+                        "earning_rate": coordinator_account_state.state.clients_state.current_epoch_rates.earning_rate,
+                        "slashing_rate": coordinator_account_state.state.clients_state.current_epoch_rates.slashing_rate,
+                    },
+                    "future": {
+                        "earning_rate": coordinator_account_state.state.clients_state.future_epoch_rates.earning_rate,
+                        "slashing_rate": coordinator_account_state.state.clients_state.future_epoch_rates.slashing_rate,
+                    },
+                },
+                "nonce": coordinator_account_state.nonce,
+            });
+
+            let treasurer_run_json = if let Some(treasurer_index) = backend
+                .resolve_treasurer_index(&run_id, treasurer_index)
+                .await?
+            {
+                let treasurer_run_address = psyche_solana_treasurer::find_run(treasurer_index);
+                let treasurer_run_state = backend.get_treasurer_run(&treasurer_run_address).await?;
+                let treasurer_run_collateral_address =
+                    spl_associated_token_account::get_associated_token_address(
+                        &treasurer_run_address,
+                        &treasurer_run_state.collateral_mint,
+                    );
+                let treasurer_run_collateral_amount = backend
+                    .get_token_amount(&treasurer_run_collateral_address)
+                    .await?;
+
+                let total_claimable_earned_points = coordinator_account_state
+                    .state
+                    .clients_state
+                    .clients
+                    .iter()
+                    .map(|client| client.earned)
+                    .sum::<u64>();
+                let total_unclaimed_earned_points =
+                    total_claimable_earned_points - treasurer_run_state.total_claimed_earned_points;
+
+                let estimated_earned_points_per_epoch = u64::try_from(
+                    coordinator_account_state
+                        .state
+                        .coordinator
+                        .epoch_state
+                        .clients
+                        .len(),
+                )
+                .unwrap()
+                    * coordinator_account_state
+                        .state
+                        .clients_state
+                        .current_epoch_rates
+                        .earning_rate;
+                let estimated_funded_epochs_count = (treasurer_run_collateral_amount
+                    - total_unclaimed_earned_points)
+                    / estimated_earned_points_per_epoch;
+
+                Some(json!({
+                    "address": treasurer_run_address.to_string(),
+                    "index": treasurer_run_state.index,
+                    "main_authority": treasurer_run_state.main_authority.to_string(),
+                    "join_authority": treasurer_run_state.join_authority.to_string(),
+                    "collateral_mint": treasurer_run_state.collateral_mint.to_string(),
+                    "funded_collateral_amount": treasurer_run_collateral_amount,
+                    "total_claimed_earned_points": treasurer_run_state.total_claimed_earned_points,
+                    "total_claimable_earned_points": total_claimable_earned_points,
+                    "total_unclaimed_earned_points": total_unclaimed_earned_points,
+                    "estimated_earned_points_per_epoch": estimated_earned_points_per_epoch,
+                    "estimated_funded_epochs_count": estimated_funded_epochs_count,
+                }))
+            } else {
+                None
+            };
+
+            println!(
+                "{}",
+                to_string_pretty(&json!({
+                    "treasurer_run": treasurer_run_json,
+                    "coordinator_instance": coordinator_instance_json,
+                    "coordinator_account": coordinator_account_json,
+                }))?
+            );
+
             Ok(())
         }
     }
