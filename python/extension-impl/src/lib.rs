c163d1ef2f6aaf2d8ac3d317f86cd0b311c45653
Ari Lotter <arilotter@gmail.com>
2025-08-13 16:50:06 -0400

    clean up silly features, fix nix python wrap

--- python/extension-impl/src/lib.rs
+++ python/extension-impl/src/lib.rs
@@ -1,14 +1,9 @@
-#[cfg(feature = "python")]
 mod extension;
 
-#[cfg(feature = "python")]
 pub fn init_embedded_python() {
-    #[cfg(not(feature = "python-extension"))]
-    {
-        extension::load_module();
-        pyo3::prepare_freethreaded_python();
-        pyo3::Python::with_gil(|py| {
-            let _ = pyo3::Python::import(py, "psyche");
-        });
-    }
+    extension::load_module();
+    pyo3::prepare_freethreaded_python();
+    pyo3::Python::with_gil(|py| {
+        let _ = pyo3::Python::import(py, "psyche");
+    });
 }
--- python/src/lib.rs
+++ python/src/lib.rs
@@ -1,2 +1,1 @@
-#[cfg(feature = "python-extension")]
 pub use psyche_python_extension_impl::*;
