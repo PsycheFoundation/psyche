3304cce04e6d7f55d9eaf58994133f1429bcbbce
Nacho Avecilla <ignacio.avecilla@lambdaclass.com>
2025-08-14 13:27:32 -0300

    Update setup test script to use permissionless runs (#197)

--- docker/test/run_owner_entrypoint.sh
+++ docker/test/run_owner_entrypoint.sh
@@ -4,24 +4,26 @@ set -o errexit
 
 solana config set --url "${RPC}"
 solana-keygen new --no-bip39-passphrase --force
+WALLET_FILE="/root/.config/solana/id.json"
 
 solana airdrop 10 "$(solana-keygen pubkey)"
 
+bash /bin/join-authorization-create.sh ${RPC} ${WALLET_FILE} 11111111111111111111111111111111
 psyche-solana-client create-run \
-    --wallet-private-key-path "/root/.config/solana/id.json" \
+    --wallet-private-key-path ${WALLET_FILE} \
     --rpc "${RPC}" \
     --ws-rpc "${WS_RPC}" \
     --run-id "${RUN_ID}"
 
 psyche-solana-client update-config \
-    --wallet-private-key-path "/root/.config/solana/id.json" \
+    --wallet-private-key-path ${WALLET_FILE} \
     --rpc "${RPC}" \
     --ws-rpc "${WS_RPC}" \
     --run-id "${RUN_ID}" \
     --config-path "/usr/local/config.toml"
 
 psyche-solana-client set-paused \
-    --wallet-private-key-path "/root/.config/solana/id.json" \
+    --wallet-private-key-path ${WALLET_FILE} \
     --rpc "${RPC}" \
     --ws-rpc "${WS_RPC}" \
     --run-id "${RUN_ID}" \
