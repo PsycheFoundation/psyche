56eb2ee19f40096589ca543945eb2126d0fab871
Dylan Socolobsky <dylan.socolobsky@lambdaclass.com>
2025-08-20 12:26:38 -0300

    evals: cmmlu

--- shared/eval/src/tasks/mod.rs
+++ shared/eval/src/tasks/mod.rs
@@ -1,5 +1,6 @@
 mod arc;
 mod boolq;
+mod cmmlu;
 mod hellaswag;
 mod mmlu;
 mod mmlu_pro;
@@ -9,6 +10,7 @@ mod piqa;
 pub use arc::ArcChallenge;
 pub use arc::ArcEasy;
 pub use boolq::BoolQ;
+pub use cmmlu::CMMLU;
 pub use hellaswag::Hellaswag;
 pub use mmlu::MMLU;
 pub use mmlu_pro::MMLUPro;
