37b4f53f519a9d662ccfc0da159ec629ab22ce5e
Ignacio Avecilla <nacho@7da1cfaf-51.cloud.together.ai>
2025-09-23 10:06:43 -0700

    Add liger kernel dependency to python dev

---
+++ python/liger-kernel.nix
@@ -0,0 +1,32 @@
+{
+  buildPythonPackage,
+  fetchFromGitHub,
+  torch,
+  lib,
+}:
+buildPythonPackage rec {
+  pname = "liger-kernel";
+  version = "0.6.2";
+  format = "setuptools";
+
+  src = fetchFromGitHub {
+    owner = "linkedin";
+    repo = "Liger-Kernel";
+    rev = "v${version}";
+    hash = "sha256-Ys3P8V6qkIucOaROsevRgnGwq0NJAJsDs6dupgPMudQ="; # You'll need to update this
+  };
+
+  propagatedBuildInputs = [
+    torch
+  ];
+
+  doCheck = false;
+
+  meta = {
+    description = "Efficient Triton kernels for LLM Training";
+    homepage = "https://github.com/linkedin/Liger-Kernel";
+    license = lib.licenses.bsd3;
+    platforms = lib.platforms.linux;
+    maintainers = with lib.maintainers; [ ];
+  };
+}
