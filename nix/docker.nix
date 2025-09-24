945629c4910d24c739301c62d2c75ce05be1001c
Dylan Socolobsky <dylan.socolobsky@lambdaclass.com>
2025-09-24 11:16:15 -0300

    build solana test client without python

--- nix/docker.nix
+++ nix/docker.nix
@@ -1,6 +1,7 @@
 {
   pkgs,
   nixglhostRustPackages,
+  nixglhostRustPackagesNoPython,
   inputs,
   externalRustPackages,
 }:
@@ -75,6 +76,60 @@ let
         busybox
         cacert
         nixglhostRustPackages."psyche-solana-client-nixglhost"
+        externalRustPackages.solana_toolbox_cli
+        jq
+
+        # Create proper system structure including /tmp
+        (pkgs.runCommand "system-setup" { } ''
+          mkdir -p $out/etc $out/tmp $out/var/tmp $out/run
+
+          # Create basic passwd and group files
+          cat > $out/etc/passwd << EOF
+            root:x:0:0:root:/root:/bin/bash
+            nobody:x:65534:65534:nobody:/nonexistent:/bin/false
+            EOF
+
+          cat > $out/etc/group << EOF
+            root:x:0:
+            nobody:x:65534:
+            EOF
+
+          # Set proper permissions for temp directories
+          chmod 1777 $out/tmp
+          chmod 1777 $out/var/tmp
+          chmod 755 $out/run
+        '')
+
+        (pkgs.runCommand "entrypoint" { } ''
+          mkdir -p $out/bin
+          cp ${../docker/test/client_test_entrypoint.sh} $out/bin/client_test_entrypoint.sh
+          cp ${../docker/test/run_owner_entrypoint.sh} $out/bin/run_owner_entrypoint.sh
+          cp ${../scripts/join-authorization-create.sh} $out/bin/join-authorization-create.sh
+          chmod +x $out/bin/client_test_entrypoint.sh
+          chmod +x $out/bin/run_owner_entrypoint.sh
+          chmod +x $out/bin/join-authorization-create.sh
+        '')
+      ];
+
+      config = {
+        Env = [
+          "NVIDIA_DRIVER_CAPABILITIES=compute,utility"
+          "NVIDIA_VISIBLE_DEVICES=all"
+        ];
+        Entrypoint = [ "/bin/client_test_entrypoint.sh" ];
+      };
+    };
+
+    docker-psyche-solana-test-client-no-python = pkgs.dockerTools.streamLayeredImage {
+      name = "psyche-solana-test-client-no-python";
+      tag = "latest";
+
+      contents = with pkgs; [
+        solana
+        bashInteractive
+        busybox
+        cacert
+        nixglhostRustPackagesNoPython."psyche-solana-client-nixglhost-no-python"
         externalRustPackages.solana_toolbox_cli
         jq
 
