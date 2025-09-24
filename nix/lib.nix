73266b5d99d9bd5af37af5bb89ce2993eba61bc2
pefontana <fontana.pedro93@gmail.com>
2025-09-24 13:05:08 -0300

    remove extra deps in nix env

--- nix/lib.nix
+++ nix/lib.nix
@@ -116,13 +116,22 @@ let
       isExample ? false,
     }:
     craneLib.buildPackage (
-      rustWorkspaceArgs
+      {
+        inherit env src;
+        strictDeps = true;
+        # Minimal dependencies for CPU-only CI testing
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          perl
+        ];
+        buildInputs = with pkgs; [
+          openssl
+        ];
+      }
       // {
         inherit cargoArtifacts;
         pname = name;
-        cargoExtraArgs =
-          (lib.optionalString (pkgs.config.cudaSupport) "--features parallelism")
-          + (if isExample then " --example ${name}" else " --bin ${name}");
+        cargoExtraArgs = if isExample then " --example ${name}" else " --bin ${name}";
         doCheck = false;
 
         meta.mainProgram = name;
