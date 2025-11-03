ab67b2f595ca1c71db19a6dc6ee717c2d7a62039
Pedro Fontana <fontana.pedro93@gmail.com>
2025-11-03 09:58:30 -0800

    Revert "call to withdraw_all in tick_round_witness when run is paused"
    This reverts commit e7f103c6dc17bbb9864fe2bb6d01e93e24dc06a6.

--- shared/coordinator/src/coordinator.rs
+++ shared/coordinator/src/coordinator.rs
@@ -983,8 +983,7 @@ impl<T: NodeIdentity> Coordinator<T> {
             // the situation where only witness nodes disconnected or everyone
             // disconnected. We just set everyone to withdrawn state and change
             // to Cooldown.
-            // Also, if the run was paused we should withdraw all clients from it
-            if num_witnesses == 0 || self.pending_pause.is_true() {
+            if num_witnesses == 0 {
                 self.withdraw_all()?;
                 self.start_cooldown(unix_timestamp);
                 return Ok(TickResult::Ticked);
@@ -995,6 +994,7 @@ impl<T: NodeIdentity> Coordinator<T> {
             if height == self.config.rounds_per_epoch - 1
                 || self.epoch_state.clients.len() < self.config.min_clients as usize
                 || num_witnesses < self.witness_quorum(num_witnesses)
+                || self.pending_pause.is_true()
             {
                 self.start_cooldown(unix_timestamp);
                 return Ok(TickResult::Ticked);
@@ -1029,6 +1029,8 @@ impl<T: NodeIdentity> Coordinator<T> {
             }
 
             if self.pending_pause.is_true() {
+                println!("withdraw_all 2");
+                self.withdraw_all()?;
                 self.change_state(unix_timestamp, RunState::Paused);
                 self.pending_pause = false.into();
                 self.epoch_state.cold_start_epoch = true.into();
