928b852ff65cb18300d1908bba7c7cfa8bf508f9
Sam Herring <samherring99@gmail.com>
2025-11-20 12:05:38 -0800

    Updating init.py with new imports

--- python/python/psyche/vllm/__init__.py
+++ python/python/psyche/vllm/__init__.py
@@ -11,15 +11,38 @@ Key Components:
 """
 
 from .engine import UpdatableLLMEngine, VLLM_AVAILABLE
+from .updater import WeightUpdater, WeightUpdate, spawn_updater_process
+from .manager import (
+    VLLMWithUpdater,
+    create_vllm_for_training,
+    create_vllm_for_atropos,
+)
+from .transforms import (
+    apply_qkv_fusion,
+    apply_gate_up_fusion,
+    apply_rotary_permute,
+    permute_for_rotary,
+    build_full_transform_config_llama,
+)
 
 __all__ = [
+    # Engine
     "UpdatableLLMEngine",
     "VLLM_AVAILABLE",
+    # Updater
+    "WeightUpdater",
+    "WeightUpdate",
+    "spawn_updater_process",
+    # Manager
+    "VLLMWithUpdater",
+    "create_vllm_for_training",
+    "create_vllm_for_atropos",
+    # Transforms
+    "apply_qkv_fusion",
+    "apply_gate_up_fusion",
+    "apply_rotary_permute",
+    "permute_for_rotary",
+    "build_full_transform_config_llama",
 ]
-
-# These will be added later
-# from .updater import WeightUpdater, spawn_updater_process
-# from .manager import VLLMWithUpdater
-# from .transforms import apply_qkv_fusion, apply_gate_up_fusion, apply_rotary_permute
 
 __version__ = "0.1.0"
