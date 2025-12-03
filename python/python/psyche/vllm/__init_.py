d94acbed25ee7b62c8facbacad8ea69eb66dd159
Sam Herring <samherring99@gmail.com>
2025-12-03 15:26:30 -0800

    Exposing testing verification method in init file

--- python/python/psyche/vllm/__init_.py
+++ python/python/psyche/vllm/__init_.py
@@ -3,6 +3,7 @@ from .engine import UpdatableLLMEngine, VLLM_AVAILABLE
 __all__ = [
     "UpdatableLLMEngine",
     "VLLM_AVAILABLE",
+    "get_shared_state_dict_from_engine",
 ]
 
 __version__ = "0.1.0"
