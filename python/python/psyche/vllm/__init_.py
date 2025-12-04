230a2ff89adddfaf344b33dc6592925f4b151bea
Sam Herring <samherring99@gmail.com>
2025-12-03 16:24:00 -0800

    Adding protocol helper methods for easy state dict broadcasting

--- python/python/psyche/vllm/__init_.py
+++ python/python/psyche/vllm/__init_.py
@@ -4,6 +4,9 @@ __all__ = [
     "UpdatableLLMEngine",
     "VLLM_AVAILABLE",
     "get_shared_state_dict_from_engine",
+    "broadcast_parameter",
+    "broadcast_state_dict",
+    "broadcast_shutdown_signal",
 ]
 
 __version__ = "0.1.0"
