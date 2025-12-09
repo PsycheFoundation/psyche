fd10953bce07152c50f69362e91bffea4470e0fa
Vincent Brunet <vincent.brunet.us@gmail.com>
2025-12-10 00:22:01 +0900

    nit

--- architectures/decentralized/solana-tooling/src/distributor.rs
+++ architectures/decentralized/solana-tooling/src/distributor.rs
@@ -18,7 +18,10 @@ use psyche_solana_distributor::logic::AirdropUpdateParams;
 use psyche_solana_distributor::logic::AirdropWithdrawParams;
 use psyche_solana_distributor::logic::ClaimCreateParams;
 use psyche_solana_distributor::logic::ClaimRedeemParams;
+use psyche_solana_distributor::state::Airdrop;
 use psyche_solana_distributor::state::AirdropMetadata;
+use psyche_solana_distributor::state::Allocation;
+use psyche_solana_distributor::state::Claim;
 use psyche_solana_distributor::state::MerkleHash;
 use psyche_solana_distributor::state::Vesting;
 use solana_sdk::instruction::Instruction;
@@ -28,8 +31,114 @@ use solana_sdk::signer::Signer;
 use solana_sdk::system_program;
 use solana_toolbox_endpoint::ToolboxEndpoint;
 
-use crate::distributor_state::find_pda_airdrop;
-use crate::distributor_state::find_pda_claim;
+pub fn find_pda_airdrop(airdrop_id: u64) -> Pubkey {
+    Pubkey::find_program_address(
+        &[Airdrop::SEEDS_PREFIX, &airdrop_id.to_le_bytes()],
+        &psyche_solana_distributor::ID,
+    )
+    .0
+}
+
+pub fn find_pda_claim(
+    airdrop: &Pubkey,
+    claimer: &Pubkey,
+    nonce: u64,
+) -> Pubkey {
+    Pubkey::find_program_address(
+        &[
+            Claim::SEEDS_PREFIX,
+            airdrop.as_ref(),
+            claimer.as_ref(),
+            nonce.to_le_bytes().as_ref(),
+        ],
+        &psyche_solana_distributor::ID,
+    )
+    .0
+}
+
+#[derive(Debug, Clone)]
+pub struct AirdropMerkleTree {
+    allocations: Vec<Allocation>,
+    merkle_layers: Vec<Vec<MerkleHash>>,
+}
+
+impl AirdropMerkleTree {
+    pub fn try_from(
+        allocations: &Vec<Allocation>,
+    ) -> Result<AirdropMerkleTree> {
+        if allocations.is_empty() {
+            return Err(anyhow::anyhow!("Allocations must not be empty"));
+        }
+        let mut merkle_layers = vec![];
+        let mut merkle_layer = vec![];
+        for allocation in allocations {
+            merkle_layer.push(allocation.to_merkle_hash());
+        }
+        merkle_layers.push(merkle_layer.clone());
+        while merkle_layer.len() > 1 {
+            let mut merkle_parents = vec![];
+            for pair in merkle_layer.chunks(2) {
+                let left = &pair[0];
+                let right = if pair.len() == 2 { &pair[1] } else { &pair[0] };
+                merkle_parents.push(MerkleHash::from_pair(left, right));
+            }
+            merkle_layers.push(merkle_parents.clone());
+            merkle_layer = merkle_parents;
+        }
+        Ok(AirdropMerkleTree {
+            allocations: allocations.to_vec(),
+            merkle_layers,
+        })
+    }
+
+    pub fn root(&self) -> Result<&MerkleHash> {
+        self.merkle_layers
+            .last()
+            .and_then(|layer| layer.first())
+            .ok_or_else(|| anyhow::anyhow!("Merkle tree is empty"))
+    }
+
+    pub fn allocations(&self) -> &[Allocation] {
+        &self.allocations
+    }
+
+    pub fn allocations_indexes_for_claimer(
+        &self,
+        claimer: &Pubkey,
+    ) -> Result<Vec<usize>> {
+        let mut indexes = vec![];
+        for (position, allocation) in self.allocations.iter().enumerate() {
+            if &allocation.claimer == claimer {
+                indexes.push(position);
+            }
+        }
+        Ok(indexes)
+    }
+
+    pub fn proof_at_allocation_index(
+        &self,
+        mut index: usize,
+    ) -> Result<Vec<MerkleHash>> {
+        let mut proof = vec![];
+        for layer in &self.merkle_layers {
+            if layer.len() == 1 {
+                break;
+            }
+            let sibling_index = if index.is_multiple_of(2) {
+                index + 1
+            } else {
+                index - 1
+            };
+            if sibling_index >= layer.len() {
+                proof.push(layer[index].clone());
+            } else {
+                proof.push(layer[sibling_index].clone());
+            }
+            index /= 2;
+        }
+        Ok(proof)
+    }
+}
 
 pub async fn process_airdrop_create(
     endpoint: &mut ToolboxEndpoint,
--- architectures/decentralized/solana-tooling/src/lib.rs
+++ architectures/decentralized/solana-tooling/src/lib.rs
@@ -1,6 +1,5 @@
 pub mod create_memnet_endpoint;
-pub mod distributor_instruction;
-pub mod distributor_state;
+pub mod distributor;
 pub mod get_accounts;
 pub mod process_authorizer_instructions;
 pub mod process_coordinator_instructions;
