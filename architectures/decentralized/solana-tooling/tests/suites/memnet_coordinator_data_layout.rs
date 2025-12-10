46f200b42a9a799389d7dba059a157b938e51a31
Ari Lotter <arilotter@gmail.com>
2025-12-10 16:34:56 -0500

    update data layout

--- architectures/decentralized/solana-tooling/tests/suites/memnet_coordinator_data_layout.rs
+++ architectures/decentralized/solana-tooling/tests/suites/memnet_coordinator_data_layout.rs
@@ -1,5 +1,5 @@
 use psyche_solana_coordinator::{
-    CoordinatorAccount, coordinator_account_from_bytes,
+    coordinator_account_from_bytes, CoordinatorAccount,
 };
 
 #[tokio::test]
@@ -25,11 +25,11 @@ pub async fn run() {
         "coordinator account data mismatch, storage may have changed"
     );
     assert_eq!(
-        coordinator_account.state.metadata.vocab_size, 32768,
+        coordinator_account.state.metadata.vocab_size, 3033,
         "coordinator account data mismatch, storage may have changed"
     );
     assert_eq!(
-        coordinator_account.nonce, 2,
+        coordinator_account.nonce, 3,
         "coordinator account data mismatch, storage may have changed"
     );
 }
