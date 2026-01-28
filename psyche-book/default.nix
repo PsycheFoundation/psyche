cde996d3c258429fd68256356c36c75c2742caa6
Ari Lotter <arilotter@gmail.com>
2026-01-28 14:28:01 -0500

    nix: ok go die book nix build im fixing u

--- psyche-book/default.nix
+++ psyche-book/default.nix
@@ -9,20 +9,6 @@
   # custom args
   rustPackages,
 }:
-let
-  # Pull crate binary package names from rustPackages
-  # prefer -nopython suffix if available, otherwise use normal version
-  allPackageNames = builtins.filter (name: !(lib.hasPrefix "test-" name)) (
-    builtins.attrNames rustPackages
-  );
-
-  binaryPackageNames = lib.unique (
-    builtins.filter (
-      name:
-      if lib.hasSuffix "-nopython" name then true else !(builtins.elem "${name}-nopython" allPackageNames)
-    ) allPackageNames
-  );
-in
 stdenvNoCC.mkDerivation {
   __structuredAttrs = true;
 
@@ -58,13 +44,22 @@ stdenvNoCC.mkDerivation {
     # we set HOME to a writable directory to avoid cache dir permission issues
     export HOME=$TMPDIR
 
-    ${lib.concatMapStringsSep "\n" (
-      name:
-      let
-        basename = lib.replaceStrings [ "-nopython" ] [ "" ] name;
-      in
-      "${rustPackages.${name}}/bin/${basename} print-all-help --markdown > generated/cli/${basename}.md"
-    ) binaryPackageNames}
+    ${lib.concatMapStringsSep "\n"
+      (
+        name:
+        let
+          basename = lib.replaceStrings [ "-nopython" ] [ "" ] name;
+        in
+        "${rustPackages.${name}}/bin/${basename} print-all-help --markdown > generated/cli/${basename}.md"
+      )
+      [
+        "psyche-centralized-local-testnet"
+        "psyche-sidecar"
+        "psyche-centralized-client"
+        "psyche-centralized-server"
+        "psyche-solana-client"
+      ]
+    }
 
     cp ${../secrets.nix} generated/secrets.nix
   '';
