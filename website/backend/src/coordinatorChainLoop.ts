9cfad6a4f63ab319567739b05010a7fd11811f38
IAvecilla <ignacio.avecilla@lambdaclass.com>
2026-01-28 15:01:50 -0300

    Bring back update_client_version to ts

--- website/backend/src/coordinatorChainLoop.ts
+++ website/backend/src/coordinatorChainLoop.ts
@@ -342,6 +342,17 @@ export async function startWatchCoordinatorChainLoop(
 						})
 						break
 					}
+					case 'update_client_version': {
+						const runPdaAddr = i.accounts[1].toString()
+						const coordinatorAddr = i.accounts[2].toString()
+						runUpdates.getAndTouchCurrentRun({
+							runPdaAddr,
+							coordinatorAddr,
+							decoded,
+							tx,
+						})
+						break
+					}
 					default: {
 						const _missed_tx: never = decoded
 						throw new Error(
