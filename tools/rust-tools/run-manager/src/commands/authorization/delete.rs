d9a2d2546afc36f26a17e3a8377017c78e0a217a
Desant pivo <pivasdesant@gmail.com>
2026-02-15 10:33:39 +0100

    Update delete.rs

--- tools/rust-tools/run-manager/src/commands/authorization/delete.rs
+++ tools/rust-tools/run-manager/src/commands/authorization/delete.rs
@@ -1,6 +1,5 @@
 use crate::commands::Command;
-use anchor_client::solana_sdk::pubkey::Pubkey;
-use anchor_client::solana_sdk::system_program;
+use crate::commands::authorization::Authorization;
 use anyhow::Result;
 use async_trait::async_trait;
 use clap::Args;
@@ -11,29 +10,15 @@ use psyche_solana_rpc::SolanaBackend;
 #[derive(Debug, Clone, Args)]
 #[command()]
 pub struct CommandJoinAuthorizationDelete {
-    #[clap(long, env, conflicts_with = "permissionless")]
-    pub authorizer: Option<Pubkey>,
-
-    /// Delete a permissionless authorization (uses system program ID)
-    #[clap(long, conflicts_with = "authorizer")]
-    pub permissionless: bool,
+    /// Authorization type: either a pubkey address or "permissionless" (maps to system program ID)
+    #[clap(long, env)]
+    pub authorization: Authorization,
 }
 
 #[async_trait]
 impl Command for CommandJoinAuthorizationDelete {
     async fn execute(self, backend: SolanaBackend) -> Result<()> {
-        let Self {
-            authorizer,
-            permissionless,
-        } = self;
-
-        let authorizer = if permissionless {
-            system_program::ID
-        } else {
-            authorizer.ok_or_else(|| {
-                anyhow::anyhow!("Either --authorizer or --permissionless must be provided")
-            })?
-        };
+        let authorizer = self.authorization.to_pubkey();
 
         let grantor = backend.get_payer();
         let grantee = authorizer;
