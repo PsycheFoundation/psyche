c01b45b394c657dd9833c6986389138081ff4eea
Desant pivo <pivasdesant@gmail.com>
2026-02-15 10:32:56 +0100

    Update mod.rs

--- tools/rust-tools/run-manager/src/commands/authorization/mod.rs
+++ tools/rust-tools/run-manager/src/commands/authorization/mod.rs
@@ -2,8 +2,10 @@ pub mod create;
 pub mod delegate;
 pub mod delete;
 pub mod read;
+pub mod types;
 
 pub use create::*;
 pub use delegate::*;
 pub use delete::*;
 pub use read::*;
+pub use types::*;
