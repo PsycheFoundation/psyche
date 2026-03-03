92f93c5e7b5f5d054fc9379007ba9273c0574792
Ari Lotter <arilotter@gmail.com>
2026-03-03 17:05:21 -0500

    refactor: move Data Server Config type into data-provider crate

--- shared/data-provider/src/remote/mod.rs
+++ shared/data-provider/src/remote/mod.rs
@@ -2,6 +2,18 @@ mod client;
 mod server;
 mod shared;
 mod tui;
+use std::path::PathBuf;
+
 pub use client::DataProviderTcpClient;
+use psyche_core::TokenSize;
 pub use server::DataProviderTcpServer;
 pub use tui::DataServerTui;
+
+use serde::{Deserialize, Serialize};
+#[derive(Serialize, Deserialize, Debug)]
+pub struct DataServerConfig {
+    pub dir: PathBuf,
+    pub token_size: TokenSize,
+    pub seq_len: usize,
+    pub shuffle_seed: [u8; 32],
+}
