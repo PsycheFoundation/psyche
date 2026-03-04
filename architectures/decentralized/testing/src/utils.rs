62f8147e2c556917a07ccbe9261fb0a35fc2fe99
IAvecilla <ignacio.avecilla@lambdaclass.com>
2026-03-04 11:14:48 -0300

    Increase warmup time for Torchtitan arch

--- architectures/decentralized/testing/src/utils.rs
+++ architectures/decentralized/testing/src/utils.rs
@@ -220,9 +220,9 @@ impl ConfigBuilder {
         self.set_value("model.LLM.checkpoint.Hub.repo_id", self.model.clone());
 
         #[cfg(feature = "python")]
-        self.set_value("config.epoch_time", 350);
+        self.set_value("config.epoch_time", 450);
         #[cfg(feature = "python")]
-        self.set_value("config.warmup_time", 300);
+        self.set_value("config.warmup_time", 400);
         #[cfg(feature = "python")]
         self.set_value("config.max_round_train_time", 30);
 
